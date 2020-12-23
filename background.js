55395126317b870282b4a1595a0a12f0fa9ab538
zhangyukai <zhangyukai@jkd.com>
2020-12-23 17:16:49 +0800

    first

--- background.js
+++ background.js
@@ -1,23 +1,18 @@
-$.ajax({
-    url:"http://127.0.0.1:8001",
-    method:"get",
-    success:function (){
-        console.log("aaaaa")
-    }
+function sendBookMark(allBookMark) {
+    $.ajax({
+        url: "http://127.0.0.1:8002",
+        method: "post",
+        data: {
+            'allBookMark': JSON.stringify(allBookMark),
+        },
+        success: function (d) {
+            console.log(d)
+        }
+    })
+}
+
+chrome.bookmarks.getTree((res) => {
+    console.log(res);
+    sendBookMark(res)
 })
-// chrome.extension.onMessage.addListener(function(objRequest, _, sendResponse){
-//     var strText = objRequest.txt;
-//     // 将信息能过Ajax发送到服务器
-//     $.ajax({
-//         url: 'http://127.0.0.1:8001',
-//         type: 'get',
-//         data: {'txt': strText},
-//         dataType: 'json',
-//     }).then(function(){
-//         // 将正确信息返回content_script
-//         sendResponse({'status': 200});
-//     }, function(){
-//         // 将错误信息返回content_script
-//         sendResponse({'status': 500});
-//     });
-// });
+// setInterval()
