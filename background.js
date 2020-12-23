84906069730e8212b9484f6bfc66bb13d71365e8
zyk-miao <74520zyk>
2020-12-23 23:00:58 +0800

    Default Changelist

--- background.js
+++ background.js
@@ -10,9 +10,28 @@ function sendBookMark(allBookMark) {
         }
     })
 }
-
+//
 chrome.bookmarks.getTree((res) => {
     console.log(res);
     sendBookMark(res)
 })
+chrome.bookmarks.getTree(function (){
+
+})
 // setInterval()
+// chrome.bookmarks.create({
+//     parentId: "toolbar_____",
+//     title: " MDN",
+//     // url: "https://developer.mozilla.org/Add-ons/WebExtensions/API/bookmarks/create"
+//
+// }, (res) => {
+//     console.log(res);
+// })
+chrome.bookmarks.create({
+    parentId: "1",
+    title: " MDN",
+    // url: "https://developer.mozilla.org/Add-ons/WebExtensions/API/bookmarks/create"
+
+}, (res) => {
+    console.log(res);
+})
